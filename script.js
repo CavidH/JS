8f0711a07e559f8f05bdf619d7c3534ab8390c7b
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-02-04 04:01:51 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var a = "salam";
+var b = 4545;
+var c = true;
+var k = [];
+var d = {};
+var f = function() {}
+    // var o = new {};
+
+
+
+document.write(a, "  =>  ", typeof(a), "<hr>")
+document.write(b, "  =>  ", typeof(b), "<hr>")
+document.write(c, "  =>  ", typeof(c), "<hr>")
+document.write(k, "  =>  ", typeof(k), "<hr>")
+document.write(d, "  =>  ", typeof(d), "<hr>")
+document.write(f, "  =>  ", typeof(f), "<hr>")
+    // document.write(o, "  =>  ", typeof(o), "<hr>")
+
+
+
+// var alpha = prompt("eded biri daxil edin");
+// var betta = prompt("eded ikini  daxil edin");
+
+// document.write((alpha + betta))
